b9edd846ba464d43b010fb4e0283d82c0b4d236c
Vikash Naveen <vikashnaveen05@gmail.com>
2026-09-12 12:42:13 +0530

    feature: validation, api documentation using swagger, log info using slf4j

--- src/main/java/com/example/demo/Todo.java
+++ src/main/java/com/example/demo/Todo.java
@@ -1,10 +1,13 @@
 package com.example.demo;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.*;
 import lombok.Data;
+import lombok.NonNull;
 
 @Entity
 @Data
@@ -12,7 +15,12 @@ public class Todo {
     @Id
     @GeneratedValue
     long id;
+    @NotBlank
+    @NotNull
+    @Schema(name="title", example="Task 1")
     String title;
+    @NotBlank
+    @NotNull
     String description;
     @Column(name="is_completed")
     boolean completed;
--- src/main/java/com/example/demo/TodoController.java
+++ src/main/java/com/example/demo/TodoController.java
@@ -1,6 +1,9 @@
 package com.example.demo;
 
 
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpHeaders;
@@ -10,7 +13,9 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Slf4j
 @RestController
+
 public class TodoController {
     @Autowired
     private TodoService tos;
@@ -19,12 +24,19 @@ public class TodoController {
     ResponseEntity<Todo> createUser(@RequestBody Todo todo){
         return new ResponseEntity<>(tos.createTodo(todo), HttpStatus.CREATED);
     }
+    @ApiResponses(value={
+            @ApiResponse(responseCode = "200",description = "Todo created Successfully"),
+            @ApiResponse(responseCode = "404",description = "Todo not found")
+    })
     @GetMapping("/{val}")
     ResponseEntity<Todo> getUserId(@PathVariable long val){
         try{
             Todo getUser=tos.getId(val);
             return new ResponseEntity<>(getUser,HttpStatus.OK);
         } catch(RuntimeException e){
+            log.info("Error");
+            log.warn("Alert");
+            log.error("",e);
             return new ResponseEntity<>((HttpHeaders) null,HttpStatus.NOT_FOUND);
         }
     }
